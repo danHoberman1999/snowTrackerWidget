e387c07074777417c1d0a95fee51c1ee1f64a389
danHoberman1999 <70783106+danHoberman1999@users.noreply.github.com>
2021-01-24 21:33:52 -0800

    Add files via upload
    Completed code for the time being

---
+++ main.js
@@ -0,0 +1,216 @@
+var schweitzer ='https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Fidaho%2Fschweitzer%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594316851%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=YdWcZLeBk8b%2F%2BTE1G1MYgrfVlJi7nsz%2BEb0%2F%2FPUEuFU%3D&amp;reserved=0'
+
+var squaw = 'https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Fcalifornia%2Fsquaw-valley-usa%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594316851%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=WGUTKphO2B91pEuYt6gIzPpa7esFNzo8MFXVrFjZXxE%3D&amp;reserved=0'
+
+var bachelor = 'https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Foregon%2Fmt-bachelor%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594316851%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=sRbk8aEFKlIp%2BXhlfGL%2BNe3k%2B8HNaOLAKg179o9abvc%3D&amp;reserved=0'
+
+var crystal = 'https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Fwashington%2Fcrystal-mountain-wa%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594316851%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=XDaqwpPk9ygCa3FEMBRJD%2F1FQsPSuZ9i6a3JWMkrM%2FY%3D&amp;reserved=0'
+
+var whistler ='https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Fbritish-columbia%2Fwhistler-blackcomb%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594316851%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=OhLu%2BGBLRXUzwsGEr8otpvF878zLjTUCy2H%2FHANmWoE%3D&amp;reserved=0'
+
+var whitewater = 'https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Fbritish-columbia%2Fwhitewater%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594326846%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=mJNzrVabcuJ6KgWOBfGcLZCid5%2BnGVXpXLW478U1Ufc%3D&amp;reserved=0'
+
+var jackson ='https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Fwyoming%2Fjackson-hole%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594326846%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=KCB%2Bop6EUhaOT57I6HWmTwBpH%2FZAGbTgLj7ONmDQ%2Bpg%3D&amp;reserved=0'
+
+var alta = 'https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Futah%2Falta-ski-area%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594326846%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=eRnH6coEJvlpcj66M1s%2FRkWJDrANg%2B0%2B4VqUDXyjw3U%3D&amp;reserved=0'
+
+var mammoth = 'https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Fcalifornia%2Fmammoth-mountain-ski-area%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594326846%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=kdfrboxrPHTxlA7ZNvih3gbz2zfHiIPjN5w7%2FAwfgco%3D&amp;reserved=0'
+
+var revelstoke = 'https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Fbritish-columbia%2Frevelstoke-mountain%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594326846%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=5MK3K%2Fhs8xx6GDZacAgiO6cXb3oJqbhx4nPBGeKlhYE%3D&amp;reserved=0'
+
+var sunValley = 'https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Fidaho%2Fsun-valley%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594326846%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=FbVYgYqUG6jY4mJJVMxYW6bAQsypbtPjdNobFnV0Mz4%3D&amp;reserved=0'
+
+var steamboat = 'https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Fcolorado%2Fsteamboat%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594326846%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=I6rLbDRIj8B485OMbFzg1BExz3s1As%2BwFkE%2B2x%2BQsh4%3D&amp;reserved=0'
+
+var aspen = 'https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Fcolorado%2Faspen-snowmass%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594326846%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=uO96fg6LCs2woFRDdRVm5uJXwBrIbBVI9SpV0DHDN5c%3D&amp;reserved=0'
+
+var winterPark = 'https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Fcolorado%2Fwinter-park-resort%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594326846%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=cS0eGmNk0LnbrTvoktccTRyUQVCclBncH%2FFmWoznJkI%3D&amp;reserved=0'
+
+var copper = 'https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Fcolorado%2Fcopper-mountain-resort%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594326846%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=vARSsE4yqyG4Bx2WNue3eGWc6gmTmiE9LB3RQ0oNs2Y%3D&amp;reserved=0'
+
+var deer = 'https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Futah%2Fdeer-valley-resort%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594326846%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=eMSEHAqDCgzCce%2Bf%2FYx3UXkUMi5OEnASdunXouyVGuM%3D&amp;reserved=0'
+
+var okemo = 'https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Fvermont%2Fokemo-mountain-resort%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594326846%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=9omba73lq3VnyjKjt5cdDwgUhTj6KSYRtkTDCGeQ4ao%3D&amp;reserved=0'
+
+var holiday ='https://nam11.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.onthesnow.com%2Fnew-york%2Fholiday-valley%2Fskireport.html&amp;data=04%7C01%7Cdhoberman%40zagmail.gonzaga.edu%7C2d67741d5303474cfe8f08d8c0f264cf%7C2ba334075ccc4940bd16ae154f04c3ca%7C1%7C0%7C637471494594336837%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=%2ByKxZa8okPpvTNmpeoNhY5YL4vn4CpLeStXcNqI88UA%3D&amp;reserved=0'
+
+var resorts = [schweitzer, squaw, bachelor, crystal, whistler, whitewater, jackson, alta, mammoth, revelstoke, sunValley, steamboat, aspen, winterPark, copper, deer, okemo, holiday]
+
+var resortNames = ['Schweitzer', 'Squaw', 'Bachelor', 'Crystal', 'Whistler', 'Whitewater', 'Jackson-hole', 'Alta', 'Mammoth', 'Revelstoke', 'Sun Valley', 'Steamboat', 'Aspen', 'Winter Park', 'Copper', 'Deer Valley', 'Okemo', 'Holiday Valley']
+
+
+var i= Math.floor(Math.random() * 18);
+i=0
+
+async function alert(){
+let alert = new Alert()
+    for(i=0; i<resortNames.length; i++){
+      alert.addAction(resortNames[i])
+    }
+    alert.addCancelAction('cancel')
+    return i = await alert.presentSheet()
+}
+
+async function upperData() {
+    const req = new Request(resorts[i]);
+    const site = await req.loadString();
+    const upper = site.match(/(sr_snow_depth_stations[^0-9]+)([0-9]+)/m);
+    try {
+        return upper
+    }
+    catch (error) {
+        console.log("error" + error);
+    }
+}
+
+async function lowerDataSquaw() {
+    const req = new Request(resorts[1]);
+    const site = await req.loadString();
+    const lower =  site.match(/(sr_snow_depth_stations[\S\s]+middle[^0-9]+)([0-9]+)/m);
+    try {
+        return lower
+    }
+    catch (error) {
+        console.log("error" + error);
+    }
+}
+
+async function lowerDataOther(){
+  const req =new Request(resorts[i])
+  const site =await req.loadString()
+  const lowerOther = site.match(/(sr_snow_depth_stations[\S\s]+middle[^0-9]+)([0-9]+)([^0-9]+)([0-9]+)/m)
+  try {
+        return lowerOther
+    }
+    catch (error) {
+        console.log("error" + error);
+    }
+}
+//
+// let t = new Timer()//
+// t.repeats= true//
+// t.timeInterval = 1000;//
+// t.schedule(()=>{
+//   timeValue(x)
+//   console.log(x)//
+// })//
+// console.log(x)
+
+function timeValue(i){
+ 
+  if(i<16){
+    i++;
+  }else{
+    i=0
+  }
+  return i;
+}
+
+async function tomorrowData() {
+    const req = new Request(resorts[i]);
+    const site = await req.loadString();
+    const tomorrow =  site.match(/(<\/script>Today[^0-9]+)([0-9])+([^0-9]+)([0-9])+([^0-9]+)([0-9])+([^0-9]+)([0-9])+([^0-9]+)([0-9])+([^0-9]+)([0-9])+([^0-9])/m);
+    try {
+        return tomorrow
+    }
+    catch (error) {
+        console.log("error" + error);
+    }
+}
+
+if(config.runsInWidget){
+   let widget = await createWidget();
+    Script.setWidget(widget);
+    Script.complete();
+}
+
+
+async function createWidget(){
+    upperValue = await upperData();
+    lowerSquawValue = await lowerDataSquaw();
+    lowerOtherValue = await lowerDataOther();
+    tomorrowValue = await tomorrowData();
+    var regex = /\d+/g;
+    var matches = tomorrowValue[0].match(regex)
+
+    let widget = new ListWidget();
+  
+
+    widget.setPadding(10,10,10,10);
+   
+    var size2 = 39
+
+    var space = 45
+    if(i===1){
+      space =40
+  }
+   
+    if(resortNames[i].length>9){
+      size2 = 33
+    }
+   
+    if(i===11 || i===8 || i=== 17)
+    {
+      size2 =29
+   }
+
+    let resortName = widget.addText(resortNames[i] + " Report");
+    resortName.font = Font.semiboldSystemFont(size2);
+    resortName.centerAlignText();
+    resortName.textColor = new Color("#ffff00")
+    widget.addSpacer(space)
+
+    const snowLevel = widget.addStack();
+    snowLevel.layoutHorizontally();
+
+    var size = 25
+    const upperSnow= snowLevel.addText("Summit: " + upperValue[upperValue.length-1] + " in");
+    upperSnow.font = Font.semiboldSystemFont(size);
+    upperSnow.centerAlignText();
+    upperSnow.textColor = new Color("#101010")
+
+    snowLevel.addSpacer();
+    snowLevel.centerAlignContent();
+
+    widget.addSpacer(35)
+
+    var location = "Base: ";
+
+
+    var lowerSnowValue = 30;
+    if(i!=1){
+       lowerSnowValue = lowerOtherValue[lowerOtherValue.length-1]
+    }else{
+       lowerSnowValue = lowerSquawValue[lowerSquawValue.length-1]
+    }
+
+
+    const snowLower = snowLevel.addText(location + lowerSnowValue + " in")
+
+
+    snowLower.font = Font.semiboldSystemFont(size);
+    snowLower.centerAlignText();
+    snowLower.textColor = new Color("#101010")
+
+    let expectedSnow = widget.addText("Today: " + matches[2] +" in")
+    expectedSnow.font = Font.semiboldSystemFont(30);
+    expectedSnow.centerAlignText();
+    expectedSnow.textColor = new Color("#101010")
+
+widget.addSpacer(15)
+
+   let dayAfter= widget.addText(matches[3]+ "/" + matches[4] + ": " + matches[5] +" in")
+      dayAfter.font =Font.semiboldSystemFont(30)
+      dayAfter.centerAlignText()
+      dayAfter.textColor = new Color("#101010")
+
+    let startColor = new Color("03588C");
+    let endColor = new Color("#F0F2F2");
+    let gradient = new LinearGradient();
+    gradient.colors = [startColor, endColor]
+    gradient.locations = [0.0, 1]
+    widget.backgroundGradient = gradient
+
+
+    return widget;
+}
